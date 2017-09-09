f306ad660542227f445e5852990e3efcfff4f394
Krasimira Badova <krasi@hacksoft.io>
2017-09-09 22:21:00 +0300

    Add bookingCreateForm

--- project/ui/src/bookingCreate.jsx
+++ project/ui/src/bookingCreate.jsx
@@ -1,9 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
+import {Cookies} from 'react-cookie';
+import {Form, Input, Label, FormGroup} from 'reactstrap';
+import VirtualizedSelect from 'react-virtualized-select';
+
+import FormMixin from './FormMixin';
 
 class BookingCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    Object.assign(FormMixin, this);
+
+    this.state = {
+      bookingCreateForm: {
+        csrfToken: new Cookies().get('csrftoken'),
+        focusedDateInput: null,
+        formErrors: [],
+        fields: {
+          room: {
+            value: {},
+            choices: [],
+            errors: [],
+            selectize: {
+              value: 'id',
+              label: 'number'
+            }
+          },
+          start_date: {
+            value: moment()
+              .add(1, 'days')
+              .format('YYYY-MM-DD'),
+            errors: []
+          },
+          end_date: {
+            value: moment()
+              .add(2, 'days')
+              .format('YYYY-MM-DD'),
+            errors: []
+          },
+          tenant: {
+            value: null,
+            errors: [],
+            choices: [],
+            selectize: {
+              value: 'id',
+              label: 'number'
+            }
+          }
+        }
+      }
+    };
+  }
+
   render() {
-    return <div>haha</div>;
+    const tenant = this.state.bookingCreateForm.fields.tenant.value;
+    const room = this.state.bookingCreateForm.fields.tenant.room;
+    const tenantChoices = this.state.bookingCreateForm.fields.tenant.choices;
+    const roomChoices = this.state.bookingCreateForm.fields.room.choices;
+
+    return (
+      <div>
+        <Form>
+          <FormGroup>
+            <DatePicker />
+            <DatePicker />
+
+            <FormGroup>
+              <Label className="control-label">ROOM</Label>
+              <VirtualizedSelect
+                options={roomChoices}
+                placeholder={'Select an available room'}
+                value={room}
+                onChange={room =>
+                  this.updateState('bookingCreateForm.fields.room.value', room)}
+              />
+              <span className="error-message">
+                {this.state.bookingCreateForm.fields.room.errors}
+              </span>
+            </FormGroup>
+            <FormGroup>
+              <Label className="control-label">TENANT</Label>
+              <VirtualizedSelect
+                options={tenantChoices}
+                placeholder={'Select a tenant'}
+                value={tenant}
+                onChange={tenant =>
+                  this.updateState(
+                    'bookingCreateForm.fields.tenant.value',
+                    tenant
+                  )}
+              />
+              <span className="error-message">
+                {this.state.bookingCreateForm.fields.tenant.errors}
+              </span>
+            </FormGroup>
+          </FormGroup>
+        </Form>
+      </div>
+    );
   }
 }
 
